007e946170147606a8f3d2c494b4f8f3db7ad522
marcodedemo <marco.dedemo@gmail.com>
2023-03-22 13:29:06 +0100

    feat: exercise done + bonus

--- js/script.js
+++ js/script.js
@@ -8,6 +8,12 @@ const { createApp } = Vue
 
       return {
 
+        // emails array
+        emails:[],
+
+        // while counter
+        counter: 0,
+
       }
 
     },
@@ -21,10 +27,36 @@ const { createApp } = Vue
     methods: {
 
 
+      createEmailAndPushOnArray(){
+
+        // creo un ciclo per creare 10 email
+        while (this.counter < 10) {
+
+          // genero una email tramite l' API
+          axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
+
+            // dichiaro una variabile che corrisponde all'email singola generata
+            let email = res.data.response;
+
+            // inserisco la singola email nell'array
+            this.emails.push(email)
+
+          });
+          
+          // aumento il contatore
+          this.counter++;
+          
+          
+        }
+      }
+
     },
 
 
     created(){
+
+      // eseguo la funzione alla creazione del documento
+      this.createEmailAndPushOnArray();
 
     },
 
